ffce4198b5253f197773937ca91cfd1dbe5230ef
hsuenlin <sean841108@gmail.com>
2021-03-16 17:14:21 +0800

    Add ReadyTrigger delay

--- Assets/Scripts/ObjectScripts/ReadyTrigger.cs
+++ Assets/Scripts/ObjectScripts/ReadyTrigger.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class ReadyTrigger : MonoBehaviour
 {
+    public float delay;
+    private bool isCountDown;
+
+    void Awake() {
+        Assert.IsFalse(isCountDown);
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if(other.name == DataManager.instance.player.name) {
-            DataManager.instance.isInReadyZone[(int)GameManager.instance.currentSceneState] = true;
+            if(GameManager.instance.gameMode == GameMode.QUEST && !isCountDown) {
+                StartCoroutine(Timer.StartTimer(delay, ()=>{
+                    DataManager.instance.isInReadyZone[(int)GameManager.instance.currentSceneState] = true;
+                }));
+                isCountDown = true;
+            }
+            else {
+                DataManager.instance.isInReadyZone[(int)GameManager.instance.currentSceneState] = true;
+            }
         }
     }
 }
