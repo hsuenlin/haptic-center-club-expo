32257272cef7c93caa8650f77cc000a2f79aa393
hsuenlin <sean841108@gmail.com>
2021-03-21 18:17:55 +0800

    Fix InstantiateOn bug

--- Assets/Scripts/Utilities/ClubUtil.cs
+++ Assets/Scripts/Utilities/ClubUtil.cs
@@ -5,8 +5,8 @@ public class ClubUtil : MonoBehaviour {
     public static GameObject InstantiateOn(GameObject _obj, Transform _transform) {
         GameObject obj = Instantiate(_obj);
         obj.transform.parent = _transform;
-        obj.transform.position = Vector3.zero;
-        obj.transform.rotation = Quaternion.identity;
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.localRotation = Quaternion.identity;
         return obj;
     }
 }
